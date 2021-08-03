3afb1d592988f0dc7b7d6ee5fa1b7ec8543906ce
andsGomes <anderson.stos.gomes@gmail.com>
2021-08-02 21:04:32 -0300

    primeiro desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,38 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    // Verificar se o campo esta vazio.
+    if(!newTaskTitle){
+       console.log('Campo vazio');
+       return;
+    }
+
+    const newTask = {
+      id: Math.floor(Math.random() * 65536),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+   
+    setTasks(task => [...task, newTask]);
+    
+    //Limpar Campo
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updateTask = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete,
+    }:task);
+    
+    setTasks(updateTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newListTasks = tasks.filter(task => task.id != id);
+    setTasks(newListTasks);
   }
 
   return (
